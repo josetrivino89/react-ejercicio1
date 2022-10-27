fa9e91fadc4736fdd209e52fa5f57ae6adb7f1f1
josetrivino89 <josetrivino89@gmail.com>
2022-10-27 13:07:05 -0300

    primer ejrcicio reacct

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Ejercicio1 from "./assets/components/Ejercicio1"
+
+
+function App() {
+   return (
+    <div className="App">
+      <Ejercicio1/>      
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/components/Ejercicio1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Ejercicio1() {
+  return (
+    
+        <p>Hello word!</p>
+      
+  )
+}
+
+export default Ejercicio1
