8ebed5ee55716bdf87ff51f3e2992af1dad3869d
josetrivino89 <josetrivino89@gmail.com>
2022-10-27 21:23:56 -0300

    se agrego Ejercicio 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Ejercicio1 from "./assets/components/Ejercicio1"
+import Ejercicio2 from "./assets/components/Ejercicio2"
 
 
 function App() {
    return (
     <div className="App">
       <Ejercicio1/>      
+      <Ejercicio2 note="My Friend"/>      
+
     </div>
   )
 }
---
+++ src/assets/components/Ejercicio2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Ejercicio2=(props)=> {
+  return (
+    <h1>Hello {props.note} 👋</h1>
+  )
+}
+
+export default Ejercicio2
